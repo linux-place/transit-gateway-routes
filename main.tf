bf545a73105bd26caa1dc3ad8f681f2c5cbc3866
Rodolfo Macbook <rodolfo.mansur@linuxplace.com.br>
2020-08-18 20:35:15 -0300

    Adicao do modulo tgw routes

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+resource "aws_ec2_transit_gateway_route_table" "route_table" {
+  transit_gateway_id = var.transit_gateway_id
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.transit_gateway_route_table_name)
+    },
+    var.tags,
+  )
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "association" {
+  count = length(var.transit_gateway_attachment_id)
+
+  transit_gateway_attachment_id  = var.transit_gateway_attachment_id[count.index]
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.route_table.id
+}
+
+resource "aws_ec2_transit_gateway_route" "route" {
+  count = length(var.transit_gateway_routes)
+
+  destination_cidr_block = lookup(var.transit_gateway_routes[count.index], "destination_cidr_block")
+  blackhole              = lookup(var.transit_gateway_routes[count.index], "blackhole", null)
+
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.route_table.id
+  transit_gateway_attachment_id  = tobool(lookup(var.transit_gateway_routes[count.index], "blackhole", false)) == false ? lookup(var.transit_gateway_routes[count.index], "attachment_id") : null
+}
+
---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "transit_gateway_id" {
+  description = "Transit Gateway ID"
+}
+
+variable "transit_gateway_route_table_name" {
+  description = "Nome da Tabela de Rotas do Transit Gateway"
+}
+
+variable "tags" {
+  description = "Tags que serão adicionadas na Tabela de Rotas do Transit Gateway"
+  type        = map
+}
+
+variable "transit_gateway_attachment_id" {
+  description = "Transit Gateway Attachment ID para a Tabela Criada"
+  type        = list
+}
+
+variable "transit_gateway_routes" {
+  description = "Informações das Rotas de VPC e qual o destinho "
+  type = list(object({
+    attachment_id          = any
+    blackhole              = bool
+    destination_cidr_block = string
+  }))
+}
