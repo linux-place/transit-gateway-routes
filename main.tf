c09aa21a24b5da783fcf4efcf315b6fbabf187e1
Rodolfo Labiapari Mansur @rodolfo_lab <rodolfo.mansur@linuxplace.com.br>
2023-01-06 11:00:49 -0300

    resource propagation

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 
 resource "aws_ec2_transit_gateway_route_table" "route_table" {
-  transit_gateway_id              = var.transit_gateway_id
-  default_propagation_route_table = var.propagation_route_table
+  transit_gateway_id = var.transit_gateway_id
 
   tags = merge(
     {
